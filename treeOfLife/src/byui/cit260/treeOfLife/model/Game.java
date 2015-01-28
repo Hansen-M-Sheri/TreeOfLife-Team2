21567950a3acfb284d159b1345a075d362434162
Sheri Hansen <sherih84015@gmail.com>
2015-01-27 23:45:34 -0700

    Game class added

---
+++ treeOfLife/src/byui/cit260/treeOfLife/model/Game.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.treeOfLife.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author sherihansen
+ */
+public class Game implements Serializable {
+    
+    //class instance variables
+    private int id;
+
+    public Game() {
+    }
+
+  
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "id=" + id + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
