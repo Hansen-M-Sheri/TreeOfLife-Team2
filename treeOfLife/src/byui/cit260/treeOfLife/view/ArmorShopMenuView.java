8da057857355fb65373ae30214a5bf0a35f810e0
Sheri Hansen <sherih84015@gmail.com>
2015-03-28 16:18:30 -0600

    individual assignment wk 11 for Sheri completed

--- treeOfLife/src/byui/cit260/treeOfLife/view/ArmorShopMenuView.java
+++ treeOfLife/src/byui/cit260/treeOfLife/view/ArmorShopMenuView.java
@@ -9,6 +9,10 @@ import byui.cit260.treeOfLife.control.GameControl;
 import byui.cit260.treeOfLife.control.ProgramControl;
 import byui.cit260.treeOfLife.model.Game;
 import byui.cit260.treeOfLife.model.GameInventoryItems;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import treeoflife.TreeOfLife;
@@ -53,7 +57,7 @@ public class ArmorShopMenuView extends View{
                 +"\n"
                 +"\nK - Sort Knowledge Points" 
                 +"\n"
-                +"\nP - Print Armor items available and items purchased"        
+                +"\nR - Print Armor items available and items purchased"        
                 +"\n"
                 +"\nG - Game Menu"
                 +"\n"
@@ -134,8 +138,7 @@ public class ArmorShopMenuView extends View{
             ArrayList<GameInventoryItems> purchaseItems = new ArrayList<>();
            purchaseItems.add(GameInventoryItems.Helmet);
             game.setPurchasedItems(purchaseItems);
-           
-            
+  
         }
         else {
         //add to purchased list
@@ -205,8 +208,15 @@ public class ArmorShopMenuView extends View{
 
         Game game = TreeOfLife.getCurrentGame();
         game.getForSale().remove(GameInventoryItems.Sword);
+       if(game.getPurchasedItems() == null){
+            ArrayList<GameInventoryItems> purchaseItems = new ArrayList<>();
+           purchaseItems.add(GameInventoryItems.Sword);
+            game.setPurchasedItems(purchaseItems);
+        }
+        else {
         //add to purchased list
         game.getPurchasedItems().add(GameInventoryItems.Sword);
+        }
           this.console.println("Your Sword has been added to your inventory");
          ArmorShopMenuView armorMenu = new ArmorShopMenuView();
         armorMenu.display();
@@ -254,6 +264,75 @@ public class ArmorShopMenuView extends View{
     }
 
     private void printInventory() {
+            //prompt for and get the name of the file to save the game in
+        this.console.println("\n\nEnter the file path for file where the game is to be saved.");
+        
+        String filePath = this.getInput();
+        
+        try {
+            ArrayList<GameInventoryItems> forSale = TreeOfLife.getCurrentGame().getForSale();
+            ArrayList<GameInventoryItems> purchasedItems = TreeOfLife.getCurrentGame().getPurchasedItems();
+            // save the game to the specified file
+            this.printItems(forSale, purchasedItems, filePath);
+        }catch(Exception ex) {
+            ErrorView.display("ArmorShopMenuView", ex.getMessage());
+        }
+        this.goToGameMenu();
+    }
+
+    private void printItems(ArrayList<GameInventoryItems> forSale, ArrayList<GameInventoryItems> purchasedItems, String filePath) {
+        try( FileOutputStream fops = new FileOutputStream(filePath)){
+            ObjectOutputStream output = new ObjectOutputStream(fops);
+         
+       String armorShopList = "\n\nArmor Inventory";
+       String armorPurchaseList = "\n\n\nPurchased Armor Items";
+            try {
+                if(forSale.size() < 1) {
+                    ErrorView.display(this.getClass().getName(), "There are no more items available in the Armor Shop");
+                }
+                else {
+
+                    //for each item in list
+                    for (GameInventoryItems item : forSale) {
+                        //get first letter of item name
+                        char armorPiece = item.name().charAt(0);
+                        //print first letter and description of item
+                        String armorDescription = item.getItemDescription();
+                        armorShopList += ("\n" + armorPiece + " - " + armorDescription);
+                    }
+                }
+                if(purchasedItems.size() < 1) {
+                    ErrorView.display(this.getClass().getName(), "You have not purchased any armor pieces yet");
+                }
+                else {
+
+                    //for each item in list
+                    for (GameInventoryItems item : purchasedItems) {
+                        //get first letter of item name
+                        char armorPiece = item.name().charAt(0);
+                        //print first letter and description of item
+                        String armorDescription = item.getItemDescription();
+                        armorPurchaseList +=("\n" + armorPiece + " - " + armorDescription);
+                    }
+
+
+                } 
+            }catch (Exception e) {
+               ErrorView.display(this.getClass().getName(), "Error reading input "+ e.getMessage());
+            }
+
+                output.writeObject(armorShopList); //write the forSale ArrayList out to file
+                output.writeObject(armorPurchaseList); 
+               
+            }
+         catch(Exception e) {
+             ErrorView.display("ArmorShopMenuView", e.getMessage());
+         }
+        //check file size
+        if(filePath.length()> 1){
+            this.console.println("Congrats! Your list has printed to the specified file.");
+            
+        }
         
     }
     
