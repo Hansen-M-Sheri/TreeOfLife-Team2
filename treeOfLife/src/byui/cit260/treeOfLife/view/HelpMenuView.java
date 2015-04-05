35aaa2e3ef28eb2bd4e13caac8f9462ddd803131
Sheri Hansen <sherih84015@gmail.com>
2015-04-04 19:08:38 -0600

    help menu options added

--- treeOfLife/src/byui/cit260/treeOfLife/view/HelpMenuView.java
+++ treeOfLife/src/byui/cit260/treeOfLife/view/HelpMenuView.java
@@ -73,23 +73,51 @@ public class HelpMenuView extends View{
     }
 
     private void helpGoalOfGame() {
-       this.console  .println("*** helpGoalOfGame function called ***");
+       this.console  .println("Throughout the course of this game, you will have "
+               + "\nopportunities to earn faith points (by answering questions and obtaining armor).  "
+               + "\nAfter you have gone through each of the levels and answered questions to "
+               + "\n demonstrate and strengthen your faith, you will be judged."
+               + "\n Your goal is to reach the highest reward, that of reaching the "
+               + "\n Tree of Life!!!!  "
+               + "\n"
+               + "\n Good luck in your journey");
     }
 
     private void helpHowToMove() {
-        this.console  .println("*** helpHowToMove function called ***");
+        this.console  .println("In the Map, you are able to enter an option to"
+                + "\nEnter Map Coordinates.  If you select this option you "
+                + "\nare asked to input the location on the map you desire to go."
+                + "\n The format of your location input must be"
+                + "\n ROW , COLUMN in order for your input to be accepted.");
      }
 
     private void helpTemple() {
-      this.console  .println("*** helpTemple function called ***");
+      this.console  .println("The Temple is a place you can go to increase your faith."
+              + "\n This is done by asking you a question of how frequently you "
+              + "\n have participated in a specific activity in the last 7 days. "
+              + "\n After answering that, you are prompted to answer the quality of "
+              + "\n your activity.  Did you perform your activity to the Best of your ability?"
+              + "\n Or was it just Good?  Or maybe even Poor?"
+                +"\n Based on your answers you will earn faith points."
+              + "\n You are allowed to visit the temple once each level.");
     }
 
     private void helpMantle() {
-        this.console  .println("*** helpMantle function called ***");
+        this.console  .println("The Mantle is a place to increase your faith through service."
+                + "\n This is done by asking you how frequently you have"
+                + "\n participated in a specific service activity in the last 7 days."
+                + "\n Based on your answers you will earn faith points. "
+                + "\n You are allowed to visit the temple once each level.");
     }
 
     private void helpArmor() {
-        this.console  .println("*** helpArmor function called ***");
+        this.console  .println("The Armor Shop contains all the armor pieces available to earn."
+                + "\n You can earn armor by obtaining faith points.  Each piece of armor requires a specific "
+                + "\n amount of faith in order to obtain it. Once you have enough faith, you can select a"
+                + "\n specific armor piece and it will be added to your personal inventory, and will "
+                + "\n increase your faith even more. "
+                + "\n Please note that when a piece of armor is added to your personal inventory,"
+                + "\n it does NOT decrease your faith points.");
     }
 
     private void returnToMainMenu() {
